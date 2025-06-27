44e76d145d2a4887ed11e878051f52599e0cdd0d
Aman Singh <amanpratap1437@gmail.com>
2025-06-27 13:19:40 +0530

    encryption & decryption of properties

--- accounts/src/main/java/com/example/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/example/accounts/AccountsApplication.java
@@ -10,8 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAware")
