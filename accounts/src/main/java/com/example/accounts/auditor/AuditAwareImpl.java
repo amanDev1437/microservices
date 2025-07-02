70d321073b5aeabb0eb130539f8ece5e447776e2
Aman Singh <amanpratap1437@gmail.com>
2025-07-02 18:56:12 +0530

    updated docker-compose.yml file

--- accounts/src/main/java/com/example/accounts/auditor/AuditAwareImpl.java
+++ accounts/src/main/java/com/example/accounts/auditor/AuditAwareImpl.java
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Component;
 import java.util.Optional;
 
 @Component("auditAware")
-public class
-AuditAwareImpl implements AuditorAware<String> {
+public class AuditAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
         return Optional.of("Aman Singh");
