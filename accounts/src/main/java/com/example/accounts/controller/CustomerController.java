d4d2cb8e86354a62eb972d2921563f7ed72389e6
Aman Singh <amanpratap1437@gmail.com>
2025-07-24 12:41:23 +0530

    implemented gateway server

--- accounts/src/main/java/com/example/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/example/accounts/controller/CustomerController.java
@@ -10,14 +10,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(
         name = "REST APIs for Customers in EazyBank",
@@ -28,6 +27,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Validated
 public class CustomerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
     private final ICustomerService customerService;
 
@@ -50,10 +51,9 @@ public class CustomerController {
     }
     )
     @GetMapping("/fetchCustomerDetails")
-    ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestParam String mobileNumber) {
-
-
-        CustomerDetailsDto customerDetailsDto = customerService.fetchCustomerDetails(mobileNumber);
+    ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("eazybank-correlation-id") String correlationId, @RequestParam String mobileNumber) {
+        logger.debug("eazybank-correlation-id found: {}", correlationId);
+        CustomerDetailsDto customerDetailsDto = customerService.fetchCustomerDetails(mobileNumber,correlationId);
         return ResponseEntity.status(HttpStatus.OK).body(customerDetailsDto);
 
     }
--- cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -16,6 +16,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -40,6 +42,7 @@ import org.springframework.web.bind.annotation.*;
 public class CardsController {
 
     private final ICardsService iCardsService;
+    private static Logger logger = LoggerFactory.getLogger(CardsController.class);
 
     @Value("${build.version}")
     private String buildVersion;
@@ -96,9 +99,10 @@ public class CardsController {
             )
     })
     @GetMapping("/fetch")
-    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestHeader("eazybank-correlation-id") String correlationId,@RequestParam
                                                                @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                                String mobileNumber) {
+        logger.debug("eazybank-correlation-id found: {}", correlationId);
         CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
